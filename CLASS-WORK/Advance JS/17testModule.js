cb22d4e1ef0ace35888114bb3eefd6d7701a9648
Vadadoriya-Harshit <harshitpatelv9@gmail.com>
2023-10-16 11:15:53 +0530

    class work

---
+++ CLASS-WORK/Advance JS/17testModule.js
@@ -0,0 +1,15 @@
+
+
+
+const Test = ()=>{
+
+    var name ="Harshit";
+    var age = 23;
+    return name + " " + age;
+}
+
+export default Test;
+
+// console.log(testFun())
+// console.log(testFun())
+// console.log(testFun())
